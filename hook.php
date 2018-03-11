cb50de7283e01598e9b549bd39ae72ab641dc927
Dennis Weber <Nijin22@users.noreply.github.com>
2018-03-11 15:14:28 +0100

    Add generation of PDFs

--- hook.php
+++ hook.php
@@ -35,17 +35,37 @@ $json = $rawPost ?: file_get_contents('php://input');
 $payload = json_decode($json);
 
 if ($payload->{'ref'} === "refs/heads/master") {
-  echo "master push!\n";
+  echo "Updating resumes...\n";
 
   // download required files:
   echo shell_exec("curl " . $downloadUrl . "resume_data.xml > " . $tmpFolder . "resume_data.xml");
   echo shell_exec("curl " . $downloadUrl . "template_de.tex > " . $tmpFolder . "template_de.tex");
   echo shell_exec("curl " . $downloadUrl . "template_en.tex > " . $tmpFolder . "template_en.tex");
+  echo shell_exec("curl " . $downloadUrl . "picture.jpg > " . $tmpFolder . "picture.jpg");
+  echo shell_exec("curl " . $resumeToLatexUrl . " > " . $tmpFolder . "resumetolatex");
+  echo "Downloads done.\n";
+
+  // create LaTeX document
+  chdir($tmpFolder);
+  echo shell_exec("python3 resumetolatex de en");
+  echo "LaTeX files created.\n";
+
+  // compile LaTeX
+  echo shell_exec("latexmk -pdf -silent filledTemplate_de.tex");
+  echo shell_exec("latexmk -pdf -silent filledTemplate_en.tex");
+  echo "LaTeX compiled.\n";
+
+  // store rendered file
+  echo shell_exec("mv filledTemplate_de.pdf " . $finalFolder . "/Dennis_Weber_Lebenslauf.pdf");
+  echo shell_exec("mv filledTemplate_en.pdf " . $finalFolder . "/Dennis_Weber_resume.pdf");
+  echo "Files stored.\n";
+
+  // cleanup
+  echo shell_exec("rm -r " . $tmpFolder . "*");
+  echo "cleanup done.\n";
 } else {
-  echo "not-master push:" . $payload->{'ref'};
+  echo "not-master push:" . $payload->{'ref' . "\n"};
 }
 
-
-echo "done."
-
+echo "all done.\n"
 ?>
